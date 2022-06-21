e0976cb5fbc8ad7b90ec9b31a87312ae363be3fc
kozyrev42 <kozyrev054@gmail.com>
2022-06-21 22:30:06 +0700

    компонент Validate в работе

---
+++ Input.php
@@ -0,0 +1,38 @@
+<?php
+
+class Input {
+// компонент проверяет на пустоту массивы $_POST и $_GET
+// компонент может возврать значения из массивов $_POST и $_GET
+
+    public static function exists($type = 'post') {
+        switch ($type) {
+
+            case 'post':
+                // проверка на пустоту массива $_POST
+                // если массив содержит, функция возвращает true, иначе false
+                return (!empty($_POST)) ? true : false;
+                // break - не используется, так как необходимо проверить еще условия  
+            case 'get': // кейс не выполняется, так как $type в аргументе определён как $type = 'post'
+                // проверка на пустоту массива $_GET
+                return (!empty($_GET)) ? true : false;
+            default:
+                // если не сработал ни один кейс
+                return false;
+            break;  
+
+        }
+    }
+
+    // в  аргументе необходимо получить ключ от массива
+    // по ключу возвращаем значение из массива
+    public static function get($item) {
+        if(isset($_POST[$item])) {
+            return $_POST[$item];
+        } else if(isset($_GET[$item])) {
+             return $_GET[$item];
+        }
+
+        // возвращаем пустую строчку, если не одно условие не сработало
+        return '';
+    }
+}
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-require_once ('Database.php');
-require_once ('Config.php');
+require_once('Database.php');
+require_once('Config.php');
+
+/*  -----------  Database  ------------ */
 
 //$users = Database::getInstance()->query("SELECT * FROM `level-two-posts` WHERE title IN (?,?)" , ['1111','ха']);
 //$users = Database::getInstance()->get('email_list', ['id', '=', '2']);
@@ -22,7 +24,6 @@ require_once ('Config.php');
 ]); */
 
 
-
 //var_dump($users->count());
 
 /* if($users->error()) {
@@ -32,13 +33,13 @@ require_once ('Config.php');
 } */
 
 
-
 /* foreach ($users->result() as $user) {
     echo '<br/>' . $user->email . '<br/>';
 } */
 
 
-/* ----------- Config */
+
+/* ----------- Config --------------*/
 // нужно создать функционал, чтобы доставать данные из массивов любой вложенность, по ключю через точку "."
 // у кода есть смысловая облочка, которую нужно понимать, зачем она нужна
 // в ооп нужно читать код блочно, а не процедурно
@@ -52,11 +53,74 @@ $GLOBALS['config'] = [
             'no' => 'yes'
         ]
     ]
-        ];
+];
 
 
 // пользоваться буду так:
 // в функцию объекта передаю ('имяМассива.ключКзначению')
 //echo Config::get('mysql.host');
 //var_dump(Config::get('mysql.something.no'));
-echo Config::get('mysql.host');
+//echo Config::get('mysql.host');
+?>
+
+<?php
+/* ---------- Validation ---- Input ----------- */
+// используем два компонента
+
+// в условии проверяем, была-ли отправлена форма
+// если проверка пройдена, начинаем функционал валидации
+if(Input::exists()){
+    // создание нового Объекта, которым будем пользоваться
+    $validate = new Validate();
+
+    // методу Объекта передаём данные на обработку: массив $_POST(что будем проверять), массив с правилами на проверку (на что будем проверять)
+    $validation = $validate->check($_POST, [
+        'username' => [ // массив содержит правила для проверки
+            'required' => true,
+            'min' => 2,
+            'max' => 15,
+            'unique' => 'users' // username должен быть уникальным в таблице 'users'
+        ],
+        'password' => [
+            'required' => true,
+            'min' => 3
+        ],
+        'password_again' => [
+            'required' => true,     // повторить пароль обязательно
+            'matches' => 'password' // должен совпадать со значение поля 'password'
+        ],
+    ]);
+
+    if ($validation->passed()) {
+        echo "валидация пройдена";
+    } else {
+        // иначе возвращаем ошибки
+        // переборка массива с ошибками
+        foreach($validation->errors() as $error){
+            echo $error . "<br>";
+        }
+    }
+}
+?>
+
+
+<form action="" method="post">
+    <div class="field">
+        <label for="">Username</label>
+        <input type="text" name="username" value="<?php echo Input::get('username') ?>">
+    </div>
+
+    <div class="field">
+        <label for="">Password</label>
+        <input type="text" name="password" >
+    </div>
+
+    <div class="field">
+        <label for="">Password again</label>
+        <input type="text" name="password_again">
+    </div>
+
+    <div class="field">
+        <button type="submit">Submit</button>
+    </div>
+</form>
