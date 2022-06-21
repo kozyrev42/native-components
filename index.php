aed0f3b97192154e1d20375b7a91628b57df69e2
kozyrev42 <kozyrev054@gmail.com>
2022-06-21 15:59:14 +0700

    готов компонент Config

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-
 require_once ('Database.php');
+require_once ('Config.php');
 
 //$users = Database::getInstance()->query("SELECT * FROM `level-two-posts` WHERE title IN (?,?)" , ['1111','ха']);
-$users = Database::getInstance()->get('email_list', ['id', '=', '2']);
+//$users = Database::getInstance()->get('email_list', ['id', '=', '2']);
 //$users = Database::getInstance()->delete('level-two-posts', ['id', '=', '3']);
 
 // задать вопрос! как я хочу пользоваться методом ?
@@ -14,12 +14,12 @@ $users = Database::getInstance()->get('email_list', ['id', '=', '2']);
     'email' => '22 еще е'
 ]); */
 
-$id = 2;
+//$id = 2;
 // хочу изменять данные в записи, по id записи
-$users = Database::getInstance()->update('email_list', $id, [
+/* $users = Database::getInstance()->update('email_list', $id, [
     'last_name' => ' 444 еще ф',
     'email' => '444 еще е'
-]);
+]); */
 
 
 
@@ -36,3 +36,27 @@ $users = Database::getInstance()->update('email_list', $id, [
 /* foreach ($users->result() as $user) {
     echo '<br/>' . $user->email . '<br/>';
 } */
+
+
+/* ----------- Config */
+// нужно создать функционал, чтобы доставать данные из массивов любой вложенность, по ключю через точку "."
+// у кода есть смысловая облочка, которую нужно понимать, зачем она нужна
+// в ооп нужно читать код блочно, а не процедурно
+$GLOBALS['config'] = [
+    'mysql' => [
+        'host' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'database' => '',
+        'something' => [
+            'no' => 'yes'
+        ]
+    ]
+        ];
+
+
+// пользоваться буду так:
+// в функцию объекта передаю ('имяМассива.ключКзначению')
+//echo Config::get('mysql.host');
+//var_dump(Config::get('mysql.something.no'));
+echo Config::get('mysql.host');
